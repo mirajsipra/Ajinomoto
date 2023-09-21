4b0335a67621bd002d4c3d0e9f1b22ce31114ed0
Faisal Mazhar <faisalmazhar.dev@gmail.com>
2023-09-21 23:05:01 +0500

    Brand, Faq, Contact, Recipe, Food Service Pages

--- src-assets/js-separate-files/components/swiper.js
+++ src-assets/js-separate-files/components/swiper.js
@@ -33,11 +33,11 @@ var collectionSwiper = new Swiper(".collections-swiper .swiper-init", {
 });
 
 function showImages(index) {
-    let allImages = document.querySelectorAll(".featured-collection__images-wrapper .image-wrapper");
-    allImages.forEach(element => {
-        element.classList.remove("show");
-    });
-    allImages[index].classList.add("show");
+  let allImages = document.querySelectorAll(".featured-collection__images-wrapper .image-wrapper");
+  allImages.forEach(element => {
+    element.classList.remove("show");
+  });
+  allImages[index].classList.add("show");
 }
 
 var featuredCollectionSwiper = new Swiper(".featured-collection--swiper .swiper-init", {
@@ -77,49 +77,69 @@ var instafeedSwiper = new Swiper(".insta-feed .swiper-init", {
 // Featured Product: pdp.html
 
 var swiper = new Swiper(".featuredProduct", {
-    slidesPerView: 3.1,
-    spaceBetween: 15,
-    loop: true,
-    grabCursor: true,
-  });
-  
-  // Product Detail Gallery: pdp.html
-  
-  var productThumbNav = new Swiper(".product-detail__ThumbsNav", {
-    slidesPerView: 6,
-    freeMode: true,
-    watchSlidesProgress: true,
-  });
-  var productMain = new Swiper(".product-detail__Main", {
-    modules: [Navigation, Thumbs],
-    loop: true,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    thumbs: {
-      swiper: productThumbNav,
-    },
-  });
-  
-  //  Product How To Use: pdp.html
-  var productusageSwiper = new Swiper(".product-usageSwiper", {
-    modules: [Navigation],
-    slidesPerView: 3.5,
-    spaceBetween: 60,
-    centeredSlides: true,
-    loop: true,
-    grabCursor: true,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
-  
-  //  Brand Story: brand-story.html
-  var brandStory = new Swiper(".brandStory", {
-    slidesPerView: 4,
-    spaceBetween: 16,
-    grabCursor: true,
-  });
-  
+  slidesPerView: 3.1,
+  spaceBetween: 15,
+  loop: true,
+  grabCursor: true,
+});
+
+// Product Detail Gallery: pdp.html
+
+var productThumbNav = new Swiper(".product-detail__ThumbsNav", {
+  slidesPerView: 6,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var productMain = new Swiper(".product-detail__Main", {
+  modules: [Navigation, Thumbs],
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: productThumbNav,
+  },
+});
+
+//  Product How To Use: pdp.html
+var productusageSwiper = new Swiper(".product-usageSwiper", {
+  modules: [Navigation],
+  slidesPerView: 3.5,
+  spaceBetween: 60,
+  centeredSlides: true,
+  loop: true,
+  grabCursor: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+//  Brand Story: brand-story.html
+var brandStory = new Swiper(".brandStory", {
+  slidesPerView: 4,
+  spaceBetween: 16,
+  grabCursor: true,
+});
+
+
+//  Product Usage on Brand Page: product-usage.html
+var productUsage = new Swiper(".productUsage", {
+  modules: [Navigation],
+  slidesPerView: 1,
+  loop: true,
+  grabCursor: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
+//  Discover More: discover-more.html
+var discoverMore = new Swiper(".discover-more__swiper .discoverMore", {
+  slidesPerView: 4,
+  spaceBetween: 16,
+  grabCursor: true,
+});
